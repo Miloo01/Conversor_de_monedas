5d9cdaa1cb9b9baa46ce99cf9d51e888cb5374dc
Camilo <cmonsalsilva@gmail.com>
2025-11-10 19:45:20 -0300

    se realiza chart.js

--- Assets/Js/chart.js
+++ Assets/Js/chart.js
@@ -1,23 +1,39 @@
+document.getElementById("consulta").addEventListener("click", async () => {
+	const moneda = document.getElementById("moneda_a_convertir").value;
+	const res = await fetch(`https://mindicador.cl/api/${moneda}`);
+	const data = await res.json();
 
-const ctx = document.getElementById('myChart');
+	// Toma los últimos 10 días
+	const serie = data.serie.slice(0, 10).reverse();  // metodo slice: devuelve una copia de una parte del array dentro de un nuevo array empezando por inicio hasta fin . reverse= invierte el orden de los elementos de una matriz y modifica la matriz original directamente
+  
+  //eje horizontal
+	const labels = serie.map((item) => item.fecha.split("T")[0]); // metodo split: divide una cadena de texto en un array de subcadenas usando un delimitador especificado. separador: El carácter o patrón que se usará como punto de corte.
+  
+  //eje vertical
+	const valores = serie.map((item) => item.valor);
+	
+  const chartData = {
+		labels,
+		datasets: [
+			{
+				label: `Historial últimos 10 días (${moneda.toUpperCase()})`,
+				borderColor: "rgb(47, 154, 248)",
+				data: valores,
+			},
+		],
+	};
 
-  new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ['2025-10-27', '2025-10-28', '2025-10-29', '2025-10-30', '2025-10-31', '2025-11-01', '2025-11-02', '2025-11-03','2025-11-04', '2025-11-05'],
-      datasets: [{
-        label: 'Historial ultimos 10 días',
-        data: [900, 910, 920, 930, 940, 955,960,975,982,990,],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
+	const config = {
+		type: "line",
+		data: chartData,
+	};
+
+	const myChart = document.getElementById("myChart");
+	myChart.style.backgroundColor = "white";
+
+	// Destruir gráfico anterior si existe
+	if (window.graficoMoneda) {
+		window.graficoMoneda.destroy();
+	}
+	window.graficoMoneda = new Chart(myChart, config);
 });
-
-
